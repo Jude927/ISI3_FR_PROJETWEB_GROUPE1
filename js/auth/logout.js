758c0645fc3ee91d3591680943c954fd3988feb8
InnaS._. <innasaida6@gmail.com>
2026-01-13 21:28:28 +0100

    Update: logout.js

--- js/auth/logout.js
+++ js/auth/logout.js
@@ -5,12 +5,12 @@ document.getElementById("logoutBtn").addEventListener("click", async () => {
   await signOut(auth);
   localStorage.clear();
   sessionStorage.clear();
-  window.location.href = "../index.html";
+  window.location.href = "#";
 });
 
 document.getElementById("logoutBtnMobile").addEventListener("click", async () => {
   await signOut(auth);
   localStorage.clear();
   sessionStorage.clear();
-  window.location.href = "../index.html";
+  window.location.href = "#";
 });
